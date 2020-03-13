2bafd336813d9febd34484cb7803059eea2790fc
Rainh <75365079@qq.com>
2020-03-13 11:23:50 +0800

    初始化项目

---
+++ src/main/java/com/test/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.test.controller;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestController implements Controller {
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("msg","重新做成的第一个github项目");
+        mav.setViewName("/first.jsp");
+        return mav;
+
+    }
+}
